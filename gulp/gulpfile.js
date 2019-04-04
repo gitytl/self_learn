5b3558784b2bb1a3db97189152e0e6fd827b7543
yangtingli <1553017655@qq.com>
2019-04-04 17:57:12 +0800

    更新

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,8 +2,6 @@ var gulp = require('gulp'),
 
     sass = require('gulp-sass'),//引用sass
 
-    livereload = require('gulp-livereload'),        //浏览器上实时更新
-
     fileinclude = require('gulp-file-include'),     //html压缩
 
     minifycss=require('gulp-minify-css'),           //css压缩;
@@ -15,53 +13,62 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync').create(), //浏览器实时刷新
     reload = browserSync.reload;
 
-var project_name='dist';
-
+var project_name='project_name';
 
 gulp.task("clean",async ()=>{
-    gulp.src('./'+project_name+'/**/*.*')
-            .pipe(clean());
+    gulp.src('./dist/'+project_name+'/**/*.*')
+        .pipe(clean());
 });
 gulp.task("watch",async ()=>{
-    livereload.listen();
     //配置浏览器路径参数
     browserSync.init({
-        server:'./'+project_name+'/', //这里指的是根目录，如果你的index.html在根目录下，会直接打开index页面，不然会显示Get Not，自己写路径就行
+        server:'./dist/'+project_name+'/', //这里指的是根目录，如果你的index.html在根目录下，会直接打开index页面，不然会显示Get Not，自己写路径就行
         port:8081  //默认打开localhost:3000,现在改成localhost:8081
     });
     gulp.watch("src/**/*.*",async (file)=>{
-        livereload.changed(file.path);
         /*html编译*/
-        gulp.src('src/project_name/*.html')
+        gulp.src('src/'+project_name+'/*.html')
             .pipe(fileinclude({
                 prefix: '@@',
                 basepath: '@file'
             }))
-            .pipe(gulp.dest(project_name))
-            .pipe(reload({stream:true}));
-        gulp.src('src/base.html')
-            .pipe(fileinclude({
-                prefix: '@@',
-                basepath: '@file'
-            }))
-            .pipe(gulp.dest('src/base'))
+            .pipe(gulp.dest('dist/'+project_name))
             .pipe(reload({stream:true}));
         /*sass编译任务*/
-        gulp.src('src/css/*.scss')
+        gulp.src('src/'+project_name+'/css/*.scss')
             .pipe(sass()) // Converts Sass to CSS with gulp-sass
             .pipe(minifycss())
-            .pipe(gulp.dest(project_name+'/css'))
+            .pipe(gulp.dest('dist/'+project_name+'/style_js'))
             .pipe(reload({stream:true}));
-        gulp.src('src/css/css/base.scss')
-            .pipe(sass()) // Converts Sass to CSS with gulp-sass
-            .pipe(minifycss())
-            .pipe(gulp.dest('src/base'))
+        gulp.src('src/'+project_name+'/css/*.js')
+            .pipe(gulp.dest('dist/'+project_name+'/style_js'))
             .pipe(reload({stream:true}));
-        /*压缩任务*/
-        gulp.src('src/images/*.*')          //监听路径
+        gulp.src('src/'+project_name+'/css/*.css')
+            .pipe(gulp.dest('dist/'+project_name+'/style_js'))
+            .pipe(reload({stream:true}));
+        /*图片压缩任务*/
+        gulp.src('src/'+project_name+'/images/*.*')          //监听路径
             .pipe(imageMin())
-            .pipe(gulp.dest(project_name+'/images')) //输出路径
+            .pipe(gulp.dest('dist/'+project_name+'/images')) //输出路径
+            .pipe(reload({stream:true}));
+        /*default*/
+        gulp.src('src/default/base.html')
+            .pipe(fileinclude({
+                prefix: '@@',
+                basepath: '@file'
+            }))
+            .pipe(gulp.dest('dist/default'))
+            .pipe(reload({stream:true}));
+        gulp.src('src/default/css/base.scss')
+            .pipe(sass()) // Converts Sass to CSS with gulp-sass
+            .pipe(minifycss())
+            .pipe(gulp.dest('dist/default'))
+            .pipe(reload({stream:true}));
+        gulp.src('src/default/baseimg/*.*')          //监听路径
+            .pipe(imageMin())
+            .pipe(gulp.dest('dist/default/baseimg')) //输出路径
             .pipe(reload({stream:true}));
     });
 });
-gulp.task("default",gulp.series('clean','watch'));
+//gulp.task("default",gulp.series('clean','watch'));
+gulp.task("default",gulp.series('watch'));
